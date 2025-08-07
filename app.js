5608794dcbe2f153035186bd8a0af94341ee12e9
Fabricio Augusto <augustofabry@gmail.com>
2025-08-07 11:38:18 -0300

    Revisión: excluir .env y node_modules del repositorio

---
+++ app.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import { sequelize } from './src/config/database.js';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Servidor funcionando');
+});
+
+const startServer = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Conectado a la base de datos MySQL con éxito.');
+
+    app.listen(PORT, () => {
+      console.log(`Servidor corriendo en http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.error('Error al conectar con la base de datos:', error);
+  }
+};
+
+startServer();
